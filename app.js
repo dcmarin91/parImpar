0e081c1c8fb92a135509914a9858d3b272b143a9
dcmarin91 <dcmarin91@hotmail.com>
2019-07-03 11:17:39 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/',(req,res) =>{
+    let lines = "";
+    for(let i=1;i<=50;i++){
+        if(i%2===0){
+            lines+=`<p>${i} Soy Par!</p>`;
+        }else{
+            lines+= `<p>${i} Soy Impar!</p>`
+        }
+    }
+    res.send(`${lines}`)
+})
+
+
+app.listen(3000,()=> console.log("Listening on port 3000"))
